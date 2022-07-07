e73f93afe827ed502bf5578793a51db51d2608b0
jeongabae <gabea52@gmail.com>
2022-07-07 21:25:15 +0900

    [create] math

--- math/bj2609_최대공약수와 최소공배수.py
+++ math/bj2609_최대공약수와 최소공배수.py
@@ -16,7 +16,7 @@ print(gcd(a,b))#최대공약수(greatest common divisor)
 print(lcm(a,b))#최소공배수 (least common multiple)
 
 #유클리드 호제법은 2개의 자연수 또는 정수의 최대공약수를 구하는 알고리즘의 하나
-# 두 자연수 a,b에서 a를 b로 나눈 나머지를 r이라고 하면 a와 b의 최대공약수는 b와 r의 최대공약수와 같다 gcd(a,b) = gcd(a,r)
+# 두 자연수 a,b에서 a를 b로 나눈 나머지를 r이라고 하면 a와 b의 최대공약수는 b와 r의 최대공약수와 같다 gcd(a,b) = gcd(b,r)
 # 그러므로, a에 b를 넣고, b에 a%b를 넣고 나머지가 0일 때까지 반복
 #gcd(24,18)=gcd(18,6)=gcd(6,0)
 #gcd(18,24)=gcd(24,6)=gcd(6,0)
