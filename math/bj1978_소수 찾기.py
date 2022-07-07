7ab2b1505a7715719973c6abf3dc92d8e71bbd77
jeongabae <gabea52@gmail.com>
2022-07-07 22:29:08 +0900

    [create] math

--- math/bj1978_소수 찾기.py
+++ math/bj1978_소수 찾기.py
@@ -1,13 +1,13 @@
-# N = int(input())
-# num = list(map(int, input().split()))
-# for i in num:
-#     if i == 1:
-#         N -= 1
-#     for j in range(2, i):
-#         if i % j == 0:
-#             N -= 1
-#             break
-# print(N)
+N = int(input())
+num = list(map(int, input().split()))
+for i in num:
+    if i == 1:
+        N -= 1
+    for j in range(2, i):
+        if i % j == 0:
+            N -= 1
+            break
+print(N)
 """짧은 다른분 풀이
 
 print(sum(all(i%j for j in range(2,i))*i>1 for i in map(int,input().split())))
