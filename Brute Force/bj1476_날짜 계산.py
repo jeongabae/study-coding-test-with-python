0ae1933d76aa0d3949662e0ead4ebd2bc1cab0e4
jeongabae <gabea52@gmail.com>
2022-07-21 19:50:51 +0900

    [create] Brute Force

--- Brute Force/bj1476_날짜 계산.py
+++ Brute Force/bj1476_날짜 계산.py
@@ -7,3 +7,9 @@ while 1:
     year+=1
 
 print(year)
+"""
+### 중국인의 나머지 정리 이용한 빠른 풀이 참고 : https://passwd.tistory.com/entry/BOJ-1476-%EB%82%A0%EC%A7%9C-%EA%B3%84%EC%82%B0
+e,s,m = map(int, input().split())
+x = (6916*e+4845*s+4200*m) % 7980
+print(x if x else 7980)
+"""
