57af333a4612e2c8a2b7de1e2bc3e91d0b6fc545
jeongabae <gabea52@gmail.com>
2022-08-06 22:12:16 +0900

    [update] N과 M (1) 다른 풀이 추가

--- DFS/bj15649_N과 M (1).py
+++ DFS/bj15649_N과 M (1).py
@@ -1,21 +1,21 @@
 import sys
 N, M = map(int, sys.stdin.readline().split())
-visited = [0]*(N+1)
+used = [0] * (N + 1)
 ans = []
 
-def dfs(cnt):
-    if cnt==M:
+def dfs(idx): #idx는 몇 번째에 있느냐를 의미 만약에 1 2 3이라는 수열이면 1은 idx=0자리 2는 idx=1, 3는 idx=3
+    if idx==M:#만약 길이가 M=3인 수열을 만드는데 idx가 3이면 0,1,2 세 개의 숫자가 찼다는거니까 수열 출력
         print(' '.join(ans))
         return
 
-    for i in range(1, N+1):
-        if not visited[i]:
-            visited[i] = 1
-            ans.append(str(i))
-            dfs(cnt+1)
-            visited[i] = 0
-            ans.pop()
-dfs(0)
+    for i in range(1, N+1):#길이가 M이 아니라면 1부터 N까지의 숫자를 확인(증가하는 순이므로 1부터)
+        if not used[i]: #중복 없어야 하므로.. 만약에 수열에 숫자i가 아직 없다면
+            used[i] = 1 #방문(사용) 처리를해주고,
+            ans.append(str(i)) #그 숫자를 수열에 넣는다.
+            dfs(idx + 1) #그리고 그 다음 수가 뭐가 올 수 있을지 찾는다.(가지치기)
+            used[i] = 0 #어떤 수열을 찾았으므로 다음 수열을 찾기 위해 수 하나를 꺼내준다. 즉, 방문(사용)하지 않았다고 표시.
+            ans.pop() # 수열에서 수를 꺼냄
+dfs(0) #0번째 수부터 길이가 M인 수열을 만들자.
 """테스트
 import sys
 N, M = map(int, sys.stdin.readline().split())
@@ -92,7 +92,7 @@ for i in case:
         print(j, end=" ")
     print()
     """
-"""순열 함수 이용 풀이2
+"""순열 함수 이용 풀이2 (1등 풀이)
 from itertools import permutations
 
 N, M = map(int, input().split())
@@ -115,4 +115,36 @@ def solve(cnt, s):
 
 
 solve(0, [])
+"""
+
+"""
+# Authored by : tony9402
+# Co-authored by : -
+# Link : http://boj.kr/69f68131effd4506a17fdac4b8569c6c
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+N, M = map(int, input().split())
+choose = [ 0 for _ in range(10) ]
+used   = [ 0 for _ in range(10) ]
+
+def dfs(cnt):
+    global N, M
+    if cnt == M:
+        for idx in range(cnt):
+            print(choose[idx], end=' ')
+        print()
+        return
+
+    for i in range(1, N + 1):
+        if used[i]:
+            continue
+        used[i] = 1
+        choose[cnt] = i
+        dfs(cnt + 1)
+        used[i] = 0
+
+dfs(0)
 """
