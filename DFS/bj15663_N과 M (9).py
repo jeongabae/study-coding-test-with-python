1cbad0c4d4a856236ba9b36c4ecc417eb81cdc88
jeongabae <gabea52@gmail.com>
2022-08-07 03:28:11 +0900

    [create] DFS, Backtracking

--- DFS/bj15663_N과 M (9).py
+++ DFS/bj15663_N과 M (9).py
@@ -15,21 +15,13 @@ def dfs(idx): #idx는 몇 번째에 있느냐를 의미 만약에 1 2 3이라는
         return
     before_num = 0
     for i in range(1, N+1):#길이가 M이 아니라면 1부터 N까지의 숫자를 확인
-        print("1 ans",ans)
-        print("1 before",before_num, "a[i-1]",a[i-1])
         if not used[i] and before_num != a[i-1]: #중복 없어야 하므로.. 만약에 수열에 숫자 a[i-1]가 아직 없다면
             used[i] = 1 #방문(사용) 처리를해주고,
             ans.append(str(a[i-1])) #그 숫자를 수열에 넣는다.
             before_num = a[i-1]
-            print("2 ans", ans)
-
-            print("2 before", before_num, "a[i-1]", a[i - 1])
             dfs(idx + 1) #그리고 그 다음 수가 뭐가 올 수 있을지 찾는다.(가지치기)
             used[i] = 0 #어떤 수열을 찾았으므로 다음 수열을 찾기 위해 수 하나를 꺼내준다. 즉, 방문(사용)하지 않았다고 표시.
             ans.pop() # 수열에서 수를 꺼냄
-            print("3 ans", ans)
-
-            print("3 before", before_num, "a[i-1]", a[i - 1])
 dfs(0) #0번째 수부터 길이가 M인 수열을 만들자.
 
 # 풀이2 : all_ans를 리스트가 아닌 dict 자료형(해쉬)으로 밤꿨더니 성공!
